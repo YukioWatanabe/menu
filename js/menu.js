0097721894d384bf546a6bc8239fe438a188d117
Fernando Watanabe <fernandoyukiowatanabe@hotmail.com>
2017-03-07 01:40:13 -0300

    Ajustes e melhorias no layout e pesquisa de programas

---
+++ js/menu.js
@@ -0,0 +1,28 @@
+$.expr[':'].containsIgnoreCase = function (n, i, m) {
+    return new RegExp(m[3],"i").test($(n).text());
+};
+
+var displayMenu = function(){
+    $('.menu').toggleClass('active');
+    $('.overlay').toggleClass('active');
+    $('.logo').toggleClass('active');
+    $('.menu .menu-group > li').removeClass('active');
+}
+
+var pesquisaMenu = function(){
+    $('.menu .menu-group > li').removeClass('active');
+    $('.menu .menu-group.nivel-1 > li').children().hide();
+    $('.menu .menu-group .item:containsIgnoreCase('+$('[name=pesquisa-menu]').val()+')').parentsUntil('.menu-group.nivel-1').find('.item').addBack().show();
+}
+
+$('.menu').on('click','.menu-group > li > span', function(){
+    var add = !$(this).parents('li').eq(0).hasClass('active');
+    $('.menu .menu-group > li').removeClass('active');
+    $(this).parents('li').addClass('active');
+    $(this).parents('li').eq(0).toggleClass('active',add);
+});
+
+$('[name=pesquisa-menu]').keyup(pesquisaMenu);
+
+$('.logo').click(displayMenu);
+$('.overlay').click(displayMenu);
